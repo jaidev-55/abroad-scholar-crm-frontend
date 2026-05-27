dad04c6a5a016b8e0aecd4b1485e555c6f0f86a0
vc <vc@vcs-MacBook-Air.local>
2026-05-27 12:42:10 +0530

    updated-settings-file

--- src/modules/settings/api/Settingsapi.ts
+++ src/modules/settings/api/Settingsapi.ts
@@ -1,5 +1,6 @@
 import type { SettingsResponse } from "../types";
-import { mockSettings } from "../utils/settingsHelpers";
+import { mockSettings } from "../utils/Settingshelpers";
+
 
 export const getSettings = async (): Promise<SettingsResponse> => {
   await new Promise((res) => setTimeout(res, 400));
